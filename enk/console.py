72abf18e77bf8ef361c58ba59eac14ab5ddf19bf
Paul Moore <p.f.moore@gmail.com>
2014-07-10 13:11:25 +0100

    Added a ScreenLine object to enk.console

--- enk/console.py
+++ enk/console.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 
 last_written = 0
 
@@ -24,3 +25,40 @@ def append_line(text):
     last_written += len(text)
     sys.stdout.write(text)
     sys.stdout.flush()
+
+class ScreenLine:
+    def __init__(self, final=""):
+        self.length, ignore = shutil.get_terminal_size()
+        # Assume the line is dirty, so clear it on first write
+        self.written = self.length - 1
+        self.final = final
+    def show(self, line):
+        # Tidy up the input. Remove trailing space, and ignore all except the
+        # last line
+        line = line.rstrip()
+        line = line.split("\n")[-1]
+        # Truncate to screen width or less
+        # Leave 4 chars, 3 for ... and 1 to avoid wrapping
+        if len(line) > self.length - 4:
+            line = line[:self.length - 4] + '...'
+        sys.stdout.write('\r')
+        text_len = len(line)
+        sys.stdout.write(line)
+        if text_len < self.written:
+            clear = self.written - text_len
+            sys.stdout.write(' ' * clear)
+            sys.stdout.write('\b' * clear)
+        self.written = text_len
+        sys.stdout.flush()
+    def finish(self, text=None):
+        if not text:
+            text = self.final
+        if text:
+            sys.stdout.write(" ")
+        sys.stdout.write(text + "\n")
+        sys.stdout.flush()
+        self.written = 0
+    def __enter__(self):
+        return self
+    def __exit__(self, exc_type, exc_value, tb):
+        self.finish()
--- tests/console_test.py
+++ tests/console_test.py
@@ -1,4 +1,4 @@
-from enk.console import overwrite_line, append_line
+from enk.console import overwrite_line, append_line, ScreenLine
 
 def test_overwrite(capsys):
     # New line - no CR
@@ -21,3 +21,61 @@ def test_overwrite(capsys):
     overwrite_line("Test 1", newline=True)
     out, err = capsys.readouterr()
     assert out == '\nTest 1'
+
+def padding(n):
+    return (" " * n) + ("\b" * n)
+
+def dummy_termsize():
+    return 10, 10
+
+def test_screenline_init(monkeypatch, capsys):
+    monkeypatch.setattr('shutil.get_terminal_size', dummy_termsize)
+    s = ScreenLine()
+    s.show("")
+    out, err = capsys.readouterr()
+    assert out == "\r" + padding(9)
+    assert err == ""
+
+def test_screenline_write(monkeypatch, capsys):
+    monkeypatch.setattr('shutil.get_terminal_size', dummy_termsize)
+    s = ScreenLine()
+    s.show("OK")
+    out, err = capsys.readouterr()
+    assert out == "\r" + "OK" + padding(7)
+    assert err == ""
+
+def test_screenline_optimise_second_write(monkeypatch, capsys):
+    monkeypatch.setattr('shutil.get_terminal_size', dummy_termsize)
+    s = ScreenLine()
+    s.show("")
+    out, err = capsys.readouterr()
+    s.show("Hello")
+    out, err = capsys.readouterr()
+    assert out == "\r" + "Hello"
+    assert err == ""
+
+def test_screenline_toolong(monkeypatch, capsys):
+    monkeypatch.setattr('shutil.get_terminal_size', dummy_termsize)
+    s = ScreenLine()
+    s.show("")
+    out, err = capsys.readouterr()
+    s.show("This is longer than six characters")
+    out, err = capsys.readouterr()
+    assert out == "\r" + "This i..."
+    assert err == ""
+
+def test_screenline_contextmgr(monkeypatch, capsys):
+    monkeypatch.setattr('shutil.get_terminal_size', dummy_termsize)
+    with ScreenLine() as s:
+        s.show("Test")
+    out, err = capsys.readouterr()
+    assert out == "\r" + "Test" + padding(5) + "\n"
+    assert err == ""
+
+def test_screenline_final(monkeypatch, capsys):
+    monkeypatch.setattr('shutil.get_terminal_size', dummy_termsize)
+    with ScreenLine(final="OK") as s:
+        s.show("Test")
+    out, err = capsys.readouterr()
+    assert out == "\r" + "Test" + padding(5) + " OK\n"
+    assert err == ""
